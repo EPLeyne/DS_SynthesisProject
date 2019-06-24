4e84effd268b24821ba7d9ba5f874e2aeeec9f25
Emmett Leyne <emmettleyne@gmail.com>
2019-06-24 13:20:23 +1000

    Changes to fastq slurm file

--- Scripts_Bash/greaves_fastqc.sh
+++ Scripts_Bash/greaves_fastqc.sh
@@ -6,16 +6,18 @@
 #SBATCH --ntasks-per-node=20
 #SBATCH --exclusive
 #SBATCH --mem=32GB
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ley015@csiro.au
 
 module load fastqc
 
 #SAMPLES=( $(cut -d " " -f 1 fastqcInputList.csv) );
-#INFILES=( $(cut -d " " -f 2 fastqcInputList.csv) );
+INFILES=( $(cut -d " " -f 2 fastqcInputList.csv) );
 
 if [ ! -z "$SLURM_ARRAY_TASK_ID" ]
 then
     i=$SLURM_ARRAY_TASK_ID
-    fastqc /OSM/CBR/AF_DATASCHOOL/input/2019-04-12_Transcritome/*.fastq.gz -o /OSM/CBR/AF_DATASCHOOL/output/epl/fastqc_results3/
+    fastqc /OSM/CBR/AF_DATASCHOOL/input/2019-04-12_Transcritome/${INFILES[i]} -o /OSM/CBR/AF_DATASCHOOL/output/epl/fastqc_results3/
 else
     echo "Error: Missing array index as SLURM_ARRAY_TASK_ID"
 f
