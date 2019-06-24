fc01f1ebd27c58bdfa7003299eea9699cec610d4
Emmett Leyne <ley015@pearcey-login.cm.hpc>
2019-06-24 13:00:40 +1000

    changed tested python file

--- Scripts_Python/csv_write.py
+++ Scripts_Python/csv_write.py
@@ -1,7 +1,10 @@
+# Writes the list of the files in a folder to a csv file.
+# Need to change to write only the .fastq.gz file names
+
 import os, csv
 
-with open("/OSM/CBR/AF_DATASCHOOL/output/epl/", 'w') as f:
+with open("/OSM/CBR/AF_DATASCHOOL/output/epl/filenames.csv", 'w') as f:
     writer = csv.writer(f)
-    for path, dirs, files in os.walk("/OSM/CBR/AF_DATASCHOOL/input/2019-04-12_Transcritome/*.fastq.gz"):
+    for path, dirs, files in os.walk("/OSM/CBR/AF_DATASCHOOL/input/2019-04-12_Transcritome/"):
         for filename in files:
-            w.writerow([filename])
+            writer.writerow([filename])
