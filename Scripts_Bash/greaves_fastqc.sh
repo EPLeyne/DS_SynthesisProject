f3900d64a3b223b51ff5a5ab73f15e2133bce655
Emmett Leyne <emmettleyne@gmail.com>
2019-06-24 13:07:27 +1000

    Added Ian sbatch script

---
+++ Scripts_Bash/greaves_fastqc.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=looptest
+#SBATCH --time=1:00:00
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=20
+#SBATCH --exclusive
+#SBATCH --mem=32GB
+
+module load fastqc
+
+#SAMPLES=( $(cut -d " " -f 1 fastqcInputList.csv) );
+#INFILES=( $(cut -d " " -f 2 fastqcInputList.csv) );
+
+if [ ! -z "$SLURM_ARRAY_TASK_ID" ]
+then
+    i=$SLURM_ARRAY_TASK_ID
+    fastqc /OSM/CBR/AF_DATASCHOOL/input/2019-04-12_Transcritome/*.fastq.gz -o /OSM/CBR/AF_DATASCHOOL/output/epl/fastqc_results3/
+else
+    echo "Error: Missing array index as SLURM_ARRAY_TASK_ID"
+f
