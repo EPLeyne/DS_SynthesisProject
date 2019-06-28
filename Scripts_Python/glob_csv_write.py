46e70303215516bea156f94502919cb325a4b855
Emmett Leyne <ley015@pearcey-login.cm.hpc>
2019-06-28 14:41:58 +1000

    test

--- Scripts_Python/glob_csv_write.py
+++ Scripts_Python/glob_csv_write.py
@@ -6,7 +6,7 @@ import csv
 globfiles = glob.glob('/OSM/CBR/AF_DATASCHOOL/input/2019-04-12_Transcritome/*.fastq.gz')
 
 
-with open("../filenames3.scv", 'w') as f:
+with open("../filenames3.csv", 'w') as f:
     writer = csv.writer(f)
     for filenames in globfiles:
         writer.writerow([filenames])
