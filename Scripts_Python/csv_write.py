aa5fcbecdc8f8375b7ffb8df5e85670c87ac8b4a
Emmett Leyne <ley015@pearcey-login.cm.hpc>
2019-06-28 14:45:08 +1000

    test

--- Scripts_Python/csv_write.py
+++ Scripts_Python/csv_write.py
@@ -3,7 +3,7 @@
 
 import os, csv
 
-with open("/OSM/CBR/AF_DATASCHOOL/output/epl/filenames.csv", 'w') as f:
+with open("../filenames3.csv", 'w') as f:
     writer = csv.writer(f)
     for path, dirs, files in os.walk("/OSM/CBR/AF_DATASCHOOL/input/2019-04-12_Transcritome/"):
         for filename in files:
